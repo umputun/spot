c1bff223e8dee1489e4a75d77d9a1760cda8bcc4
Umputun <umputun@gmail.com>
2023-05-04 20:07:23 -0500

    add implementation of secrets for vault and aws secrets manager

---
+++ app/secrets/spot.go
@@ -0,0 +1,184 @@
+package secrets
+
+import (
+	"crypto/rand"
+	"database/sql"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql" // mysql driver loaded here
+	_ "github.com/lib/pq"              // postgres driver loaded here
+	_ "modernc.org/sqlite"             // sqlite driver loaded here
+
+	"golang.org/x/crypto/argon2"
+	"golang.org/x/crypto/nacl/secretbox"
+)
+
+// InternalProvider is a secret provider that stores secrets in a database, encrypted.
+// supported database types: sqlite, postgres, mysql
+type InternalProvider struct {
+	db     *sql.DB
+	key    []byte
+	dbType string // add dbType field to struct
+}
+
+// NewInternalProvider creates a new InternalProvider.
+func NewInternalProvider(connString string, key []byte) (*InternalProvider, error) {
+	dbType := func(connString string) (string, error) {
+		if strings.HasPrefix(connString, "postgres://") {
+			return "postgres", nil
+		}
+		if strings.Contains(connString, "@tcp(") {
+			return "mysql", nil
+		}
+		if strings.HasPrefix(connString, "file:/") {
+			return "sqlite", nil
+		}
+		return "", fmt.Errorf("unsupported database type in connection string")
+	}
+
+	dbt, err := dbType(connString)
+	if err != nil {
+		return nil, fmt.Errorf("can't determine database type: %w", err)
+	}
+
+	db, err := sql.Open(dbt, connString)
+	if err != nil {
+		return nil, fmt.Errorf("error opening secrets database: %w", err)
+	}
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS spot_secrets (skey VARCHAR(255) PRIMARY KEY, sval TEXT);`)
+	if err != nil {
+		return nil, err
+	}
+	return &InternalProvider{db: db, dbType: dbt, key: key}, nil
+}
+
+// Get retrieves a secret from the database, decrypts it, and returns it.
+func (p *InternalProvider) Get(key string) (string, error) {
+	var encryptedData []byte
+
+	loadStmt := "SELECT sval FROM spot_secrets WHERE skey = ?"
+	if p.dbType == "postgres" {
+		loadStmt = "SELECT sval FROM spot_secrets WHERE skey = $1"
+	}
+	stmt, err := p.db.Prepare(loadStmt)
+	if err != nil {
+		return "", err
+	}
+	defer stmt.Close()
+
+	if err = stmt.QueryRow(key).Scan(&encryptedData); err != nil {
+		if err == sql.ErrNoRows {
+			return "", errors.New("secret not found")
+		}
+		return "", err
+	}
+
+	decrypted, err := p.decrypt(string(encryptedData))
+	if err != nil {
+		return "", fmt.Errorf("can't get secret for %s: %w", key, err)
+	}
+
+	return decrypted, nil
+}
+
+// Set stores a secret in the database, encrypted.
+func (p *InternalProvider) Set(key, value string) error {
+	encryptedData, err := p.encrypt(value)
+	if err != nil {
+		return fmt.Errorf("can't set secret for %s: %w", key, err)
+	}
+
+	// use database-specific "INSERT" statements
+	var insertStmt string
+	switch p.dbType {
+	case "sqlite":
+		insertStmt = "INSERT OR REPLACE INTO spot_secrets (skey, sval) VALUES ($1, $2)"
+	case "postgres":
+		insertStmt = "INSERT INTO spot_secrets (skey, sval) VALUES ($1, $2) ON CONFLICT (skey) DO UPDATE SET sval = $2;"
+	case "mysql":
+		insertStmt = "REPLACE INTO spot_secrets (skey, sval) VALUES (?, ?)"
+	default:
+		return fmt.Errorf("unsupported database type: %s", p.dbType)
+	}
+
+	stmt, err := p.db.Prepare(insertStmt)
+	if err != nil {
+		return fmt.Errorf("error preparing insert statement: %w", err)
+	}
+	defer stmt.Close()
+
+	if _, err = stmt.Exec(key, encryptedData); err != nil {
+		return fmt.Errorf("error inserting secret: %w", err)
+	}
+	return nil
+}
+
+// Delete removes a secret from the database.
+func (p *InternalProvider) Delete(key string) error {
+	deleteStmt := "DELETE FROM spot_secrets WHERE skey = ?"
+	if p.dbType == "postgres" {
+		deleteStmt = "DELETE FROM spot_secrets WHERE skey = $1"
+	}
+	stmt, err := p.db.Prepare(deleteStmt)
+	if err != nil {
+		return fmt.Errorf("error preparing delete statement: %w", err)
+	}
+	defer stmt.Close()
+
+	if _, err := stmt.Exec(key); err != nil {
+		return fmt.Errorf("error deleting secret for %s: %w", key, err)
+	}
+	return nil
+}
+
+func (p *InternalProvider) encrypt(data string) (string, error) {
+	salt := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
+		return "", err
+	}
+
+	keyBytes := deriveKey(p.key, salt)
+	naclKey := new([32]byte)
+	copy(naclKey[:], keyBytes)
+
+	nonce := new([24]byte)
+	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
+		return "", err
+	}
+
+	out := make([]byte, 24+16)
+	copy(out, nonce[:])
+	copy(out[24:], salt)
+
+	sealed := secretbox.Seal(out, []byte(data), nonce, naclKey)
+	return base64.StdEncoding.EncodeToString(sealed), nil
+}
+
+func (p *InternalProvider) decrypt(encodedData string) (string, error) {
+	sealed, err := base64.StdEncoding.DecodeString(encodedData)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := new([24]byte)
+	copy(nonce[:], sealed[:24])
+
+	salt := sealed[24:40]
+	keyBytes := deriveKey(p.key, salt)
+	naclKey := new([32]byte)
+	copy(naclKey[:], keyBytes)
+
+	decrypted, ok := secretbox.Open(nil, sealed[40:], nonce, naclKey)
+	if !ok {
+		return "", errors.New("failed to decrypt")
+	}
+	return string(decrypted), nil
+}
+
+func deriveKey(key, salt []byte) []byte {
+	return argon2.IDKey(key, salt, 1, 64*1024, 4, 32)
+}
--- go.mod
+++ go.mod
@@ -3,23 +3,41 @@ module github.com/umputun/spot
 go 1.20
 
 require (
+	github.com/aws/aws-sdk-go-v2/config v1.18.23
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.22
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.19.7
 	github.com/fatih/color v1.15.0
 	github.com/go-pkgz/fileutils v0.2.0
 	github.com/go-pkgz/lgr v0.11.0
 	github.com/go-pkgz/syncs v1.3.0
+	github.com/go-sql-driver/mysql v1.7.1
 	github.com/hashicorp/go-multierror v1.1.1
+	github.com/hashicorp/vault/api v1.9.1
 	github.com/jessevdk/go-flags v1.5.0
+	github.com/lib/pq v1.10.9
 	github.com/pelletier/go-toml/v2 v2.0.7
 	github.com/pkg/sftp v1.13.5
 	github.com/stretchr/testify v1.8.2
 	github.com/testcontainers/testcontainers-go v0.19.0
 	golang.org/x/crypto v0.8.0
 	gopkg.in/yaml.v3 v3.0.1
+	modernc.org/sqlite v1.22.1
 )
 
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.18.0 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.33 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.27 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.27 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.11 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
+	github.com/cenkalti/backoff/v3 v3.0.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/containerd/containerd v1.7.0 // indirect
 	github.com/cpuguy83/dockercfg v0.3.1 // indirect
@@ -28,16 +46,27 @@ require (
 	github.com/docker/docker v23.0.4+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.6.6 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.1.6 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/kr/text v0.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/moby/patternmatcher v0.5.0 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
 	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
@@ -47,13 +76,27 @@ require (
 	github.com/opencontainers/runc v1.1.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	golang.org/x/tools v0.8.0 // indirect
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/grpc v1.54.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 	gotest.tools/v3 v3.4.0 // indirect
+	lukechampine.com/uint128 v1.2.0 // indirect
+	modernc.org/cc/v3 v3.40.0 // indirect
+	modernc.org/ccgo/v3 v3.16.13 // indirect
+	modernc.org/libc v1.22.5 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/opt v0.1.3 // indirect
+	modernc.org/strutil v1.1.3 // indirect
+	modernc.org/token v1.0.1 // indirect
 )
