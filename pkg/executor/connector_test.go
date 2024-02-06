57b38be73d322015f656f3fad7e7d8695ab4ed7a
Umputun <umputun@gmail.com>
2024-02-06 01:11:37 -0600

    add tests

--- pkg/executor/connector_test.go
+++ pkg/executor/connector_test.go
@@ -39,4 +39,18 @@ func TestConnector_Connect(t *testing.T) {
 		_, err = c.Connect(ctx, "127.0.0.1:12345", "h1", "test")
 		require.ErrorContains(t, err, "failed to dial: dial tcp 127.0.0.1:12345")
 	})
+
+	t.Run("timeout", func(t *testing.T) {
+		c, err := NewConnector("testdata/test_ssh_key", time.Nanosecond, MakeLogs(true, false, nil))
+		require.NoError(t, err)
+		_, err = c.Connect(ctx, hostAndPort, "h1", "test")
+		require.ErrorContains(t, err, "i/o timeout")
+	})
+
+	t.Run("unreachable host", func(t *testing.T) {
+		c, err := NewConnector("testdata/test_ssh_key", time.Second, MakeLogs(true, false, nil))
+		require.NoError(t, err)
+		_, err = c.Connect(ctx, "10.255.255.1:22", "h1", "test")
+		require.ErrorContains(t, err, "failed to dial: dial tcp 10.255.255.1:22: i/o timeout")
+	})
 }
