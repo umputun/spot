52d3c791383ab2f358c173fc38f6a9599a0d2c76
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-02-17 20:12:00 +0000

    use verbose completions for zsh and fish descriptions

--- completions/spot-secrets.fish
+++ completions/spot-secrets.fish
@@ -1,2 +1,2 @@
 # fish completion for spot-secrets (generated via go-flags)
-complete -c spot-secrets -a '(GO_FLAGS_COMPLETION=1 spot-secrets (commandline -cop) 2>/dev/null)'
+complete -c spot-secrets -a '(GO_FLAGS_COMPLETION=verbose spot-secrets (commandline -cop) 2>/dev/null | string replace -r "\\s+# " "\t")'
--- completions/spot.fish
+++ completions/spot.fish
@@ -1,2 +1,2 @@
 # fish completion for spot (generated via go-flags)
-complete -c spot -a '(GO_FLAGS_COMPLETION=1 spot (commandline -cop) 2>/dev/null)'
+complete -c spot -a '(GO_FLAGS_COMPLETION=verbose spot (commandline -cop) 2>/dev/null | string replace -r "\\s+# " "\t")'
