ead267c700d5c2496b4fa1b8134b3226a296e03e
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-02-17 20:12:00 +0000

    use verbose completions for zsh and fish descriptions

--- completions/spot-secrets.bash
+++ completions/spot-secrets.bash
@@ -1,8 +1,7 @@
 # bash completion for spot-secrets (generated via go-flags)
 _spot_secrets() {
     local args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
-    local IFS=$'\n'
-    COMPREPLY=($(GO_FLAGS_COMPLETION=1 "${COMP_WORDS[0]}" "${args[@]}" 2>/dev/null))
+    mapfile -t COMPREPLY < <(GO_FLAGS_COMPLETION=1 "${COMP_WORDS[0]}" "${args[@]}" 2>/dev/null)
     return 0
 }
 complete -o default -F _spot_secrets spot-secrets
